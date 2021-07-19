38f48b785d9ad1377785395d304af32720da1240
SyrusVir <jude.alnas@gmail.com>
2021-07-19 13:04:41 -0500

    Function renaming

--- hfss3DModeler/hfssModeler.py
+++ hfss3DModeler/hfssModeler.py
@@ -1,6 +1,6 @@
 from hfss3DModeler import _getModeler
 
-def hfssGetObjName(o3DMod, obj_idx):
+def GetObjName(o3DMod, obj_idx):
     """
     :param o3DMod: oDesign.SetActiveEditor("3D Modeler")
     :param obj_idx: Object index (i.e. order of creation)
@@ -9,7 +9,7 @@ def hfssGetObjName(o3DMod, obj_idx):
     return o3DMod.GetObjectName(obj_idx)
 
 
-def hfssGetNumObjects(o3DMod):
+def GetNumObjects(o3DMod):
     """
     :param o3DMod: oDesign.SetActiveEditor("3D Modeler")
     :return: Returns number of objects in design
@@ -17,7 +17,7 @@ def hfssGetNumObjects(o3DMod):
     return o3DMod.GetNumObjects()
 
 
-def hfssGetAllObjNames(o3DMod):
+def GetAllObjNames(o3DMod):
     """
     :param o3DMod: oDesign.SetActiveEditor("3D Modeler")
     :return: List of names of all objects in target design
@@ -25,7 +25,7 @@ def hfssGetAllObjNames(o3DMod):
     return [o3DMod.GetObjectName(i) for i in range(o3DMod.GetNumObjects())]
 
 
-def hfssDelete(o3DMod, objects):
+def Delete(o3DMod, objects):
     """
     :param o3DMod: oDesign.SetActiveEditor("3D Modeler")
     :param objects: List or comma-separated string of objects to delete
